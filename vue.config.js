a133729c1caac1ca7640ce984d5dd52333d6488c
Marcus Brown <mb@formula-micro.dk>
2019-09-10 14:03:31 +0200

    Added build configuration and deleted Vuetify logo

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-    chainWebpack: config => {
-      config.module
-        .rule("i18n")
-        .resourceQuery(/blockType=i18n/)
-        .type("javascript/auto")
-        .use("i18n")
-        .loader("@kazupon/vue-i18n-loader")
-        .end();
+  chainWebpack: config => {
+    config.module
+      .rule("i18n")
+      .resourceQuery(/blockType=i18n/)
+      .type("javascript/auto")
+      .use("i18n")
+      .loader("@kazupon/vue-i18n-loader")
+      .end();
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "NSearch",
+        win: {
+          target: ["nsis", "portable"],
+          // icon: __dirname + "/src/Assets/icon.ico"
+        }
+      }
     }
+  },
 }
